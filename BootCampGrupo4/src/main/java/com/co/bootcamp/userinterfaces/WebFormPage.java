62498c08d1461f7d0e93fbb96941400c37c280f6
FelipeCarrr <101758804+FelipeCarrr@users.noreply.github.com>
2022-11-24 16:06:09 -0500

    Update WebFormPage.java

--- BootCampGrupo4/src/main/java/com/co/bootcamp/userinterfaces/WebFormPage.java
+++ BootCampGrupo4/src/main/java/com/co/bootcamp/userinterfaces/WebFormPage.java
@@ -5,6 +5,7 @@ import net.serenitybdd.screenplay.targets.Target;
 public class WebFormPage {
     public static final Target TXT_ENTRADA = Target.the("ingresa el campo Entrada de texto").locatedBy("//input[@type='text']");
     public static final Target TXT_CLAVE = Target.the("ingresa el campo Clave").locatedBy("//input[@type='password']");
+    public static final Target TXT_AREA = Target.the("ingresa texto en el campo textarea").locatedBy("//textarea");
     public static final Target BTN_SEND = Target.the("Click en el boton submit").locatedBy("//*[text()='Submit']");
 
 }
